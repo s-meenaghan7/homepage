55ad0254deab726f291acd2206c806564a3b784e
Sean Meenaghan <seanmeenaghan@gmail.com>
2025-08-19 13:06:27 -0700

    feat: initial intro/hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Container } from "@mui/material"
 import NavBar from "./components/navbar";
+import IntroSection from "./components/intro-section";
 
 export default function App() {
     return (
         <>
             <NavBar />
             <Container>
-                hello world
+                <IntroSection />
             </Container>
         </>
     );
