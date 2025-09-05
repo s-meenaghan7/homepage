48538fe5553f77bc1d20cde3b0d3d84d49d495e3
Sean Meenaghan <seanmeenaghan@gmail.com>
2025-09-04 19:25:24 -0700

    fix: FooterLink wrapped in parent Box to prevent stretching parent container

--- src/features/footer/FooterLink.tsx
+++ src/features/footer/FooterLink.tsx
@@ -1,3 +1,4 @@
+import Box from "@mui/material/Box"
 import Link from "@mui/material/Link"
 import type { NavButtonConfig } from "../navigation"
 
@@ -5,15 +6,16 @@ type FooterLinkProps = Pick<NavButtonConfig, "id" | "label" | "onClick">
 
 export function FooterLink({ id, label, onClick }: FooterLinkProps) {
   return (
-    <Link
-      display="block"
-      underline="hover"
-      variant="subtitle2"
-      alignItems="center"
-      onClick={(e) => onClick(e, id)}
-      sx={{ cursor: "pointer", my: 0.5 }}
-    >
-      {label}
-    </Link>
+    <Box>
+      <Link
+        underline="hover"
+        variant="subtitle2"
+        alignItems="center"
+        onClick={(e) => onClick(e, id)}
+        sx={{ cursor: "pointer", my: 0.5 }}
+      >
+        {label}
+      </Link>
+    </Box>
   )
 }
