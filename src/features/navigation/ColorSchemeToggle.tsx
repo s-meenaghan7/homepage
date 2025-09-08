8cc3ad0b778955c0834ba6f9695a3e7f9fb7656d
Sean Meenaghan <seanmeenaghan@gmail.com>
2025-09-07 18:43:46 -0700

    feat: ColorSchemeToggle more dynamic tool tip text.
    The tool tip now states 'Use <opoosite mode> mode' on enter. On click, the tool tip text confirms that the mode has been changed appropriately.

--- src/features/navigation/ColorSchemeToggle.tsx
+++ src/features/navigation/ColorSchemeToggle.tsx
@@ -3,9 +3,11 @@ import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined"
 import IconButton from "@mui/material/IconButton"
 import { useColorScheme } from "@mui/material/styles"
 import Tooltip from "@mui/material/Tooltip"
+import { useState } from "react"
 
 export function ColorSchemeToggle() {
   const { mode, setMode } = useColorScheme()
+  const [tooltipText, setTooltipText] = useState("")
 
   const toggleMode = (mode: "light" | "dark" | "system" | undefined) => {
     return mode === "dark" ? "light" : "dark"
@@ -14,15 +16,18 @@ export function ColorSchemeToggle() {
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
 
-    setMode(toggleMode(mode))
+    const newMode = toggleMode(mode)
+    setMode(newMode)
+    setTooltipText(`Using ${newMode} mode!`)
   }
 
   if (!mode) return null
 
   return (
     <Tooltip
-      title={`Change to ${toggleMode(mode)} mode`}
       enterDelay={100}
+      title={tooltipText}
+      onOpen={() => setTooltipText(`Use ${toggleMode(mode)} mode`)}
     >
       <IconButton onClick={handleClick}>
         {mode === "dark" ? <LightModeOutlinedIcon /> : <DarkModeOutlinedIcon />}
