9fa77bd29a40d76b495f2aa571af16cb30cb4893
Sean Meenaghan <seanmeenaghan@gmail.com>
2025-09-30 11:29:08 -0700

    fix: improved responsiveness of Contact section components; ContactInformation displays InfoBadges on larger screens, Social buttons on smaller screens; reduced Contact section header size from h3 to h4

--- src/features/contact/ContactInformation.tsx
+++ src/features/contact/ContactInformation.tsx
@@ -1,13 +1,22 @@
+import Email from "@mui/icons-material/Email"
+import GitHub from "@mui/icons-material/GitHub"
+import LinkedIn from "@mui/icons-material/LinkedIn"
 import Grid from "@mui/material/Grid"
 import Stack from "@mui/material/Stack"
+import { useTheme } from "@mui/material/styles"
 import Typography from "@mui/material/Typography"
+import useMediaQuery from "@mui/material/useMediaQuery"
+import { InfoBadge } from "../../common"
 import { EmailButton } from "../../common/EmailButton"
 import { GitHubButton } from "../../common/GitHubButton"
 import { LinkedInButton } from "../../common/LinkedInButton"
 
 export function ContactInformation() {
+  const theme = useTheme()
+  const isMediumScreen = useMediaQuery(theme.breakpoints.down("md"))
+
   return (
-    <Grid size={{ xs: 12, md: 5 }}>
+    <Grid justifyItems="center">
       <Typography
         variant="h6"
         gutterBottom
@@ -15,13 +24,38 @@ export function ContactInformation() {
         Contact Information
       </Typography>
       <Stack
-        direction="row"
+        direction={{ xs: "row", sm: "row", md: "column" }}
         spacing={2}
         fontSize="large"
       >
-        <EmailButton />
-        <LinkedInButton />
-        <GitHubButton />
+        {isMediumScreen ? (
+          <>
+            <EmailButton />
+            <LinkedInButton />
+            <GitHubButton />
+          </>
+        ) : (
+          <>
+            <InfoBadge
+              title="Email"
+              icon={<Email />}
+              value="seanmeenaghan@gmail.com"
+              href="mailto:seanmeenaghan@email.com"
+            />
+            <InfoBadge
+              title="LinkedIn"
+              icon={<LinkedIn />}
+              value="linkedin.com/in/seanmeenaghan/"
+              href="https://www.linkedin.com/in/seanmeenaghan/"
+            />
+            <InfoBadge
+              title="GitHub"
+              icon={<GitHub />}
+              value="github.com/s-meenaghan7"
+              href="https://github.com/s-meenaghan7"
+            />
+          </>
+        )}
       </Stack>
     </Grid>
   )
