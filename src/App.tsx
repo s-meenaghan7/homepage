641eb660d3c30cf0bb2641b9b75cef48b78ba37f
Sean Meenaghan <seanmeenaghan@gmail.com>
2025-10-18 18:55:27 -0700

    refactor: created Home component for main page flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import Container from "@mui/material/Container"
-import { About } from "./features/about"
-import { Contact } from "./features/contact"
-import { Experience } from "./features/experience"
 import { Footer } from "./features/footer"
-import { Intro } from "./features/intro"
 import { NavBarContainer } from "./features/navigation"
-import { Projects } from "./features/projects"
-import { VisitorCounter } from "./features/visitor-counter"
+import { Home } from "./Home"
 
 export default function App() {
   return (
     <>
       <NavBarContainer />
-      <Container>
-        <Intro />
-        <About />
-        <Experience />
-        <Projects />
-        <Contact />
-        <VisitorCounter />
-      </Container>
+      <Home />
       <Footer />
     </>
   )
