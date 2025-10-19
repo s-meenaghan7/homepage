8232d636d86492513fc69cb31cb1a9ec56096fe7
Sean Meenaghan <seanmeenaghan@gmail.com>
2025-10-18 20:41:41 -0700

    feat: added react-router w/ Routes in the App component. Added react-markdown, using new MarkdownRenderer component to render markdown from static files with the CaseStudy component, loading the corresponding markdown file from URL param slug.
    CaseStudy component JSX is WIP.

---
+++ src/common/MarkdownRenderer.tsx
@@ -0,0 +1,94 @@
+import Box from "@mui/material/Box"
+import Link from "@mui/material/Link"
+import Typography from "@mui/material/Typography"
+import ReactMarkdown from "react-markdown"
+
+export interface MarkdownRendererProps {
+  markdown: string
+}
+
+export function MarkdownRenderer({ markdown }: MarkdownRendererProps) {
+  return (
+    <Box
+      sx={{
+        "& pre": {
+          backgroundColor: "#f5f5f5",
+          padding: 2,
+          borderRadius: 2,
+          overflowX: "auto",
+        },
+        "& code": {
+          fontFamily: "monospace",
+          fontSize: "0.9rem",
+        },
+        "& table": {
+          width: "100%",
+          borderCollapse: "collapse",
+          marginY: 2,
+        },
+        "& th, & td": {
+          border: "1px solid #ccc",
+          padding: "8px",
+        },
+      }}
+    >
+      <ReactMarkdown
+        components={{
+          h1: ({ children }) => (
+            <Typography
+              variant="h4"
+              gutterBottom
+            >
+              {children}
+            </Typography>
+          ),
+          h2: ({ children }) => (
+            <Typography
+              variant="h5"
+              gutterBottom
+            >
+              {children}
+            </Typography>
+          ),
+          h3: ({ children }) => (
+            <Typography
+              variant="h6"
+              gutterBottom
+            >
+              {children}
+            </Typography>
+          ),
+          p: ({ children }) => <Typography variant="body1">{children}</Typography>,
+          a: ({ href, children }) => (
+            <Link
+              href={href}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {children}
+            </Link>
+          ),
+          li: ({ children }) => (
+            <li>
+              <Typography
+                component="span"
+                variant="body1"
+              >
+                {children}
+              </Typography>
+            </li>
+          ),
+          code({ children }) {
+            return (
+              <Box component="pre">
+                <code>{children}</code>
+              </Box>
+            )
+          },
+        }}
+      >
+        {markdown}
+      </ReactMarkdown>
+    </Box>
+  )
+}
