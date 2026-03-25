b564ec92607841f02c895df97d4a99896e33bd34
Sean Meenaghan <seanmeenaghan@gmail.com>
2026-03-24 17:22:01 -0700

    fix: small updates to MarkdownContent class: tocHeadings method now returns a mapping of titles to slugs

--- src/features/blog/markdown.test.ts
+++ src/features/blog/markdown.test.ts
@@ -1,17 +1,17 @@
 import { describe, expect, it } from "vitest"
 import { MarkdownContent } from "./markdown"
 
-describe("MarkdownContent", () => {
+describe("MarkdownContent.tocHeadings", () => {
   it("should read markdown content and parse the table of contents", () => {
     const markdownContent =
       "# Heading 1\nSome content here\n## Subheading 1\nMore content"
     const markdown = new MarkdownContent(markdownContent)
     const toc = markdown.tocHeadings()
 
-    const expectedToc = [
-      "- [Heading 1](#heading-1)",
-      "  - [Subheading 1](#subheading-1)",
-    ]
+    const expectedToc = new Map([
+      ["Heading 1", "heading-1"],
+      ["Subheading 1", "subheading-1"],
+    ])
 
     expect(toc).toEqual(expectedToc)
   })
